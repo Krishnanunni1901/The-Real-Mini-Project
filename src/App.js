d7da69737de3a2b488827b4d83b2d1c8750701cb
krishnanunni1901 <krishnanunni1901@gmail.com>
2021-03-29 11:29:58 +0530

    krish changed

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
 import React, { useState, useEffect } from "react";
+import Table from './Components/Table'
 
 
 function App() {
-
-
-  const [value, setValue] = useState([]);
-
-  const fetchData = () => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      
-      .then((Data) => setValue(Data));
-      
-
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  console.log(value);
   return (
-    <div className="App">
-      <ol>
-        {value.map(post => 
-          
-            <li>
-              {post.id}-{post.title}
-            </li>
-          
-        )}
-      </ol>
-    </div>
+   <Table/>
   );
 }
 
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,41 +1,40 @@
 import React,{useState,useEffect} from 'react'
-import Tableitems from '../components/Tableitems'
-import Medium from '../components/Medium'
-import High from '../components/High'
-import Low from '../components/Low'
-import Open from '../components/Open'
-import Closed from '../components/Closed'
+import Tableitems from './Tableitems'
+import Medium from './Medium'
+import High from './High'
+import Low from './Low'
+import Open from './Open'
+import Closed from './Closed'
 import './Tablestyle.css'
-import {Divider} from '@material-ui/core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSortDown } from '@fortawesome/free-solid-svg-icons'
 export default function Table() {
-    // const [posts,setposts]=useState([])
-    // useEffect(() => {
-    //     const url='https://jsonplaceholder.typocode.com/posts';
-    //     fetch(url).then(resp => resp.json())
-    //     .then(resp=>setposts(resp))
-    // },[]
-    // )
+    const [posts,setposts]=useState([])
+    useEffect(() => {
+        const url='https://jsonplaceholder.typocode.com/posts';
+        fetch(url).then(resp => resp.json())
+        .then(resp=>setposts(resp))
+    },[]
+    )
+    
+
     return (
         <div>
             
 <table>
 
 <tr>
-  <th className="column margin">ID<FontAwesomeIcon icon={faSortDown} className="downicon"></FontAwesomeIcon></th>
-  <th className="column">Request by<FontAwesomeIcon icon={faSortDown}className="downicon"></FontAwesomeIcon> </th>
-  <th className="column">Subject<FontAwesomeIcon icon={faSortDown}className="downicon"></FontAwesomeIcon></th>  
-  <th className="column"> Assignee<FontAwesomeIcon icon={faSortDown}className="downicon"></FontAwesomeIcon></th>  
-  <th className="column">Priority<FontAwesomeIcon icon={faSortDown}className="downicon"></FontAwesomeIcon></th>   
-  <th className="column">Status<FontAwesomeIcon icon={faSortDown}className="downicon"></FontAwesomeIcon></th>
-  <th className="column">Create Date<FontAwesomeIcon icon={faSortDown}className="downicon"></FontAwesomeIcon></th>
-  <th className="column"> Due Date<FontAwesomeIcon icon={faSortDown}className="downicon"></FontAwesomeIcon></th> 
-  <th className="column">Action<FontAwesomeIcon icon={faSortDown}className="downicon"></FontAwesomeIcon></th>
+  <th className="column margin">ID<FontAwesomeIcon  className="downicon"></FontAwesomeIcon></th>
+  <th className="column">Request by<FontAwesomeIcon className="downicon"></FontAwesomeIcon> </th>
+  <th className="column">Subject<FontAwesomeIcon className="downicon"></FontAwesomeIcon></th>  
+  <th className="column"> Assignee<FontAwesomeIcon className="downicon"></FontAwesomeIcon></th>  
+  <th className="column">Priority<FontAwesomeIcon className="downicon"></FontAwesomeIcon></th>   
+  <th className="column">Status<FontAwesomeIcon className="downicon"></FontAwesomeIcon></th>
+  <th className="column">Create Date<FontAwesomeIcon className="downicon"></FontAwesomeIcon></th>
+  <th className="column"> Due Date<FontAwesomeIcon className="downicon"></FontAwesomeIcon></th> 
+  <th className="column">Action<FontAwesomeIcon className="downicon"></FontAwesomeIcon></th>
 </tr>
 <br></br>
  
-<Tableitems id={"#12345"} pic={"https://via.placeholder.com/600/24f355"} request={"Jose.D"}  subject={'Support for theme'} assignee={"https://via.placeholder.com/600/24f355"} priority={<Medium/>} status={<Open/>} createDate={'01/01/2021'} dueDate={'31/01/2021'} />
+<Tableitems id={"#12345"} pic={"https://via.placeholder.com/600/24f355"} request={posts[0].name}  subject={'Support for theme'} assignee={"https://via.placeholder.com/600/24f355"} priority={<Medium/>} status={<Open/>} createDate={'01/01/2021'} dueDate={'31/01/2021'} />
 <Tableitems id={"#12345"} pic={"https://via.placeholder.com/600/24f355"}request={'Jose.D'} subject={'Support for theme'} assignee={"https://via.placeholder.com/600/24f355"} priority={<Low/>} status={<Closed/>} createDate={'01/01/2021'} dueDate={'31/01/2021'} />
 <Tableitems id={"#12345"} pic={"https://via.placeholder.com/600/24f355"}request={'Jose.D'} subject={'Support for theme'} assignee={"https://via.placeholder.com/600/24f355"} priority={<High/>} status={<Open/>} createDate={'01/01/2021'} dueDate={'31/01/2021'} />
 <Tableitems id={"#12345"} pic={"https://via.placeholder.com/600/24f355"}request={'Jose.D'} subject={'Support for theme'} assignee={"https://via.placeholder.com/600/24f355"} priority={<Medium/>} status={<Closed/>} createDate={'01/01/2021'} dueDate={'31/01/2021'} />
